82b3f40e7fda41baa5cbf5af0d586086735b7b15
Glitch (deeply-fir) <none>
2019-02-04 12:29:02 +0000

    ⏳🐕 Checkpoint ./server.js:2457780/456 ./package.json:2457780/37

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ var cors = require('cors');
 
 // require and use "multer"...
 var multer  = require('multer');
-var upload = multer({ dest: 'uploads/' });
+var upload = multer();
+var bodyParser = require("body-parser");
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
@@ -19,6 +21,10 @@ app.get('/', function (req, res) {
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
+
+app.post('/api/fileanalyse', upload.single('file'), (req, res, next) =>{
+  res.json(req.file);
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
